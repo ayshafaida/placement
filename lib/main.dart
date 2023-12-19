200f4430963ffd31bf497964e6c1b1dff42d344e
ayshafaida <ayshafaida.m@gmail.com>
2023-12-19 17:29:52 +0530

    department field added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:final_project/homepage.dart';
-import 'package:final_project/myprofile.dart';
 import 'package:final_project/signup.dart';
 import 'package:flutter/material.dart';
 
@@ -15,8 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home:Login(),
-      home:myprofile(),
+      home:Login(),
+      // home:myprofile(),
     );
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -10,7 +10,9 @@ class Signup extends StatefulWidget {
 
 class _SignupState extends State<Signup> {
   String radioButtonItem = 'ONE';
+  String department = 'BA.English';
   int id = 1;
+  List<String> dept = ['BA.English', 'BA.Sociology', 'BSc.Computer Science'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,73 +72,76 @@ class _SignupState extends State<Signup> {
               ),
               Text('Gender'),
               Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Radio(
-              value: 1,
-              groupValue: id,
-              onChanged: (val) {
-                setState(() {
-                  radioButtonItem = 'Male';
-                  id = 1;
-                });
-              },
-            ),
-            Text(
-              'Male',
-              style: new TextStyle(fontSize: 17.0),
-            ),
- 
-            Radio(
-              value: 2,
-              groupValue: id,
-              onChanged: (val) {
-                setState(() {
-                  radioButtonItem = 'Female';
-                  id = 2;
-                });
-              },
-            ),
-            Text(
-              'Female',
-              style: new TextStyle(
-                fontSize: 17.0,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Radio(
+                    value: 1,
+                    groupValue: id,
+                    onChanged: (val) {
+                      setState(() {
+                        radioButtonItem = 'Male';
+                        id = 1;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Male',
+                    style: new TextStyle(fontSize: 17.0),
+                  ),
+                  Radio(
+                    value: 2,
+                    groupValue: id,
+                    onChanged: (val) {
+                      setState(() {
+                        radioButtonItem = 'Female';
+                        id = 2;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Female',
+                    style: new TextStyle(
+                      fontSize: 17.0,
+                    ),
+                  ),
+                  Radio(
+                    value: 3,
+                    groupValue: id,
+                    onChanged: (val) {
+                      setState(() {
+                        radioButtonItem = 'Others';
+                        id = 3;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Others',
+                    style: new TextStyle(fontSize: 17.0),
+                  ),
+                ],
               ),
-            ),
-            
-            Radio(
-              value: 3,
-              groupValue: id,
-              onChanged: (val) {
-                setState(() {
-                  radioButtonItem = 'Others';
-                  id = 3;
-                });
-              },
-            ),
-            Text(
-              'Others',
-              style: new TextStyle(fontSize: 17.0),
-            ),
-          ],
-        ),
-         const Text('Department'),
+              const Text('Department'),
               const SizedBox(
                 height: 5,
               ),
-              TextField(
-                // controller: emailController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your department',
-                ),
-              ),
+              DropdownButton(
+                  items: dept.map((String value) {
+                    return DropdownMenuItem<String>(
+                      child: new Text(value),
+                      value: value,
+                    );
+                  }).toList(),
+                  hint: const Text('Select Dept'),
+                  onChanged: (newValue) {
+                    setState(() {
+                      department = newValue!;
+                    });
+                  }),
               const Text('Email'),
               const SizedBox(
                 height: 5,
               ),
               TextField(
-                // controller: emailController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: 'Enter Email',
@@ -164,12 +169,11 @@ class _SignupState extends State<Signup> {
               ),
               //create button for register
               ElevatedButton(
-                onPressed: () {
-                },
+                onPressed: () {},
                 child: InkWell(
                   onTap: () {
-                     Navigator.push(
-                         context, MaterialPageRoute(builder: (_) => Login()));
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Login()));
                   },
                   child: Text(
                     'Register',
