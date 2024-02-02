d1e68687a9221642c579c5f2d4d5fa7fab24d23f
ayshafaida <ayshafaida.m@gmail.com>
2024-02-02 20:28:37 +0530

    jobs added

--- lib/job.dart
+++ lib/job.dart
@@ -8,121 +8,109 @@ class Jobdetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple[200],
-          elevation: 0,
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back_ios,
-            ),
-            onPressed: () => Navigator.pop(context),
+      appBar: AppBar(
+         backgroundColor: Colors.purple[200],
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
           ),
-          title: Text(
-            "Company Name",
-            // company!.companyName!,
-          ),
+          onPressed: () => Navigator.pop(context),
         ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                    width: double.infinity,
-                    height: 100,
-                    child: Container(
-                      alignment: Alignment(0.0, 2.5),
-                      child: CircleAvatar(
+        title: Text("Company Name",
+          // company!.companyName!,
+        ),
+      ),
+      
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                     width: double.infinity,
+                     height: 100,
+                     child: Container(
+                       alignment: Alignment(0.0, 2.5),
+                       child: CircleAvatar(
                         backgroundImage: AssetImage("images/img.jpg"),
                         radius: 40.0,
-                      ),
-                    )),
-                SizedBox(
-                  height: 35,
-                ),
-                Text(
-                  "Flutter Developer",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-                Column(
-                  children: [
-                    Divider(
-                      height: 80,
-                      color: Colors.grey[200],
-                      thickness: 1,
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "Zoople IT Company Software & Networking",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 13,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Salary: ",
-                      style: TextStyle(
+                       ),
+                       ) ),
+                       SizedBox(height: 35,),
+                       Text(
+                        "Flutter Developer",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                        ),
+                        ),
+                        Column(
+                          children: [
+                            Divider(
+                              height: 80,
+                              color: Colors.grey[200],
+                              thickness: 1,
+                            )
+                          ],
+                        ),
+                      SizedBox(height:5,),
+                       Text(
+                        "Zoople IT Company Software & Networking",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold
+                        ),
+                        ),
+                        SizedBox(height: 13,
+                        ),
+                         Text(
+                        "Salary:20000",
+                        style: TextStyle(
                           color: Colors.black,
                           fontSize: 15,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "20000",
-                      style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: 20.0, right: 0.0, top: 0.0, bottom: 0.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Job Description",
-                      style: TextStyle(
-                        color: Colors.grey[900],
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
-                        decorationColor: Colors.grey,
-                        decorationThickness: 2,
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    "As a Flutter Developer,you will be in charge of reviewing the software specificat and UI mockups,developing a cross-browser mobile application",
-                    // maxLines: 10,
-                    // overflow:
-                    // TextOverflow.ellipsis,
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),
-                    //specifications and UI mockups,developing a cross-browser mobile application'
-                  ),
-                )
-              ],
-            ),
+                          fontWeight: FontWeight.bold
+                        ),
+                        ),
+                         SizedBox(height: 30,
+                         ),
+                        
+                        Padding(
+                          padding:  EdgeInsets.only(left: 20.0,right: 0.0,top: 0.0,bottom: 0.0),
+                          child: Row( crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                              "Job Description",
+                              style: TextStyle(
+                                color: Colors.grey[900],
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Colors.grey,
+                                decorationThickness: 2, 
+                                ),
+                              ),
+                          
+                              
+                              Padding(
+                                padding:  EdgeInsets.all(20),
+                                child: Text(
+                                  "As a Flutter Developre,you will be in charge of reviewing the software specificat and UI mockups,developing a cross-browser mobile application",
+                                  // maxLines: 10,
+                                  // overflow: 
+                                 // TextOverflow.ellipsis,
+                                  style: TextStyle(fontSize: 10.0,fontWeight: FontWeight.w500),
+                                  //specifications and UI mockups,developing a cross-browser mobile application'
+                                 ),
+                              )
+                            ],
+                          ),
+                        )
+                        
+            ],
           ),
-        ));
+        ),
+      ) );
   }
 }
